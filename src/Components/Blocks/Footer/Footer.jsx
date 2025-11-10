a5d3301d565b1f2a21b9a02678a1535c1f023b5f
alimdzhatdoev <alimdzhatdoev1@gmail.com>
2025-11-10 15:22:04 +0300

    Update

--- src/Components/Blocks/Footer/Footer.jsx
+++ src/Components/Blocks/Footer/Footer.jsx
@@ -2,11 +2,61 @@ import React from "react";
 import classes from './Footer.module.css';
 
 function Footer({ children, ...props }) {
-    return ( 
-        <>
-            Footer
-        </>
-     );
+    return (
+        <div className={classes.footerBack}>
+            <div className={classes.footerCenter}>
+                <div className={classes.footerBack_top}>
+                    <div className={classes.footerBack_top_item}>
+                        <div className={classes.footerBack_top_item_line}>
+                            <div className={classes.footerBack_top_item_line_title}>МИР РПЛ</div>
+                            <div className={classes.footerBack_top_item_line_link}>Таблица</div>
+                            <div className={classes.footerBack_top_item_line_link}>Матчи </div>
+                            <div className={classes.footerBack_top_item_line_link}>Новости </div>
+                            <div className={classes.footerBack_top_item_line_link}>Видео </div>
+                            <div className={classes.footerBack_top_item_line_link}>Клубы </div>
+                            <div className={classes.footerBack_top_item_line_link}>Игроки </div>
+                            <div className={classes.footerBack_top_item_line_link}>Статистика </div>
+                            <div className={classes.footerBack_top_item_line_link}>Трансляции </div>
+                        </div>
+                        <div className={classes.footerBack_top_item_line}>
+                            <div className={classes.footerBack_top_item_line_title}>Российская Премьер-Лига</div>
+                            <div className={classes.footerBack_top_item_line_link}>О лиге</div>
+                            <div className={classes.footerBack_top_item_line_link}>Арбитры </div>
+                            <div className={classes.footerBack_top_item_line_link}>Главные тренеры </div>
+                            <div className={classes.footerBack_top_item_line_link}>Спонсоры и партнеры </div>
+                            <div className={classes.footerBack_top_item_line_link}>Билеты </div>
+                            <div className={classes.footerBack_top_item_line_link}>Карта болельщика </div>
+                            <div className={classes.footerBack_top_item_line_link}>Контакты </div>
+                            <div className={classes.footerBack_top_item_line_link}>Антидопинг </div>
+                            <div className={classes.footerBack_top_item_line_link}>Медицина </div>
+                        </div>
+                        <div className={classes.footerBack_top_item_line}>
+                            <div className={classes.footerBack_top_item_line_title}>Документы</div>
+                            <div className={classes.footerBack_top_item_line_link}>Регламенты</div>
+                            <div className={classes.footerBack_top_item_line_link}>Информация </div>
+                            <div className={classes.footerBack_top_item_line_link}>Антидопинг </div>
+                        </div>
+                    </div>
+                    <div className={classes.footerBack_top_item}>
+                        <div className={classes.footerBack_top_item_line}>
+                            <div className={classes.footerBack_top_item_line_title}>Социальные сети</div>
+                            <div className={classes.footerBack_top_item_line_soc}>
+                                <div className={classes.footerBack_top_item_line_soc_block}><img src="tg.png" alt="" /></div>
+                                <div className={classes.footerBack_top_item_line_soc_block}><img src="yt.png" alt="" /></div>
+                                <div className={classes.footerBack_top_item_line_soc_block}><img src="vk.png" alt="" /></div>
+                                <div className={classes.footerBack_top_item_line_soc_block}><img src="tt.png" alt="" /></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={classes.footerBack_bottom}>
+                    <div className={classes.footerBack_bottom_elem}>© 2001-2025 Молодежная любительская футбольная лига</div>
+                    <div className={classes.footerBack_bottom_elem}>Политика обработки персональных данных</div>
+                    <div className={classes.footerBack_bottom_elem}><img src="ru.png" alt="" /></div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Footer;
--- src/Components/Pages/Main_Page.jsx
+++ src/Components/Pages/Main_Page.jsx
@@ -123,6 +123,7 @@ function Main_Page({ children, ...props }) {
 
     return (
         <>
+            {/* Прошедшие / будущие матчи */}
             <div className={classes.match}>
                 <div className={classes.matchBtns}>
                     <div className={`${classes.matchBtns_item} ${isActive == true ? classes.activBtn : ''}`}
@@ -148,6 +149,7 @@ function Main_Page({ children, ...props }) {
                 </div>
             </div>
 
+            {/* Новости */}
             <div className={classes.centerBlock}>
                 <div className={classes.news_and_stats}>
 
@@ -558,13 +560,285 @@ function Main_Page({ children, ...props }) {
                 </div>
             </div>
 
-            <div className={classes.centerBlock} style={{backgroundColor: '#2A0054'}}>
+            {/* Спонсоры */}
+            <div className={classes.centerBlock} style={{ backgroundColor: '#2A0054' }}>
                 <div className={classes.sponsors}>
                     <img src="s1.png" alt="" />
                     <img src="s2.png" alt="" />
                     <img src="s3.png" alt="" />
                     <img src="s4.png" alt="" />
                     <img src="s5.png" alt="" />
+                </div>
+            </div>
+
+            {/* Статистика */}
+            <div className={classes.centerBlock}>
+                <div className={classes.statistic}>
+                    <div className={classes.statistic_title}>
+                        <div className={classes.title_block}>Статистика МЛФ Лиги 2025/26</div>
+                        <div className={classes.statistic_title_moreLink}>Больше данных <img src="/arrowRight.png" alt="" /></div>
+                    </div>
+                    <div className={classes.statistic_table}>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_title_img}>
+                                    <img src="table1.png" alt="" />
+                                </div>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Сыгранные матчи</div>
+                                    <div className={classes.statistic_table_item_info_stats}>111</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Победы</div>
+                                    <div className={classes.statistic_table_item_info_stats}>77</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Ничьи</div>
+                                    <div className={classes.statistic_table_item_info_stats}>34</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Общая посещаемость</div>
+                                    <div className={classes.statistic_table_item_info_stats}>1 519 855</div>
+                                </div>
+                            </div>
+                        </div>
+                        {/*  */}
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_title_img}>
+                                    <img src="tableBall.png" alt="" />
+                                </div>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Голы</div>
+                                    <div className={classes.statistic_table_item_info_stats}>293</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Голы (среднее за матч)</div>
+                                    <div className={classes.statistic_table_item_info_stats}>2.64</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Пенальти</div>
+                                    <div className={classes.statistic_table_item_info_stats}>45</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Пенальти реализованные</div>
+                                    <div className={classes.statistic_table_item_info_stats}>33</div>
+                                </div>
+                            </div>
+                        </div>
+                        {/*  */}
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_title_img}>
+                                    <img src="tableFoals.png" alt="" />
+                                </div>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Фолы (среднее за матч)</div>
+                                    <div className={classes.statistic_table_item_info_stats}>29</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Желтые карточки</div>
+                                    <div className={classes.statistic_table_item_info_stats}>484 <img src="/tableYellow.png" alt="" /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Двойные желтые карточки</div>
+                                    <div className={classes.statistic_table_item_info_stats}>16 <img src="/tableYellowRed.png" alt="" /></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={classes.statistic_table_item}>
+                            <div className={classes.statistic_table_item_title}>
+                                <div className={classes.statistic_table_item_info}>
+                                    <div className={classes.statistic_table_item_info_name}>Красные карточки</div>
+                                    <div className={classes.statistic_table_item_info_stats}>9 <img src="/tableRed.png" alt="" /></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Видео */}
+            <div className={classes.centerBlock}>
+                <div className={classes.videos}>
+                    <div className={classes.statistic_title}>
+                        <div className={classes.title_block}>Популярные видео</div>
+                        <div className={classes.statistic_title_moreLink}>Все видео<img src="/arrowRight.png" alt="" /></div>
+                    </div>
+
+                    <div className={classes.videos_items}>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="videoPreview.png" alt="" />
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_tag}>Обзоры</div>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Спартак-Москва 2-2 Зенит</div>
+                        </div>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="videoPreview.png" alt="" />
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_tag}>Обзоры</div>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Спартак-Москва 2-2 Зенит</div>
+                        </div>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="videoPreview.png" alt="" />
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_tag}>Обзоры</div>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Спартак-Москва 2-2 Зенит</div>
+                        </div>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="videoPreview.png" alt="" />
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_tag}>Обзоры</div>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Спартак-Москва 2-2 Зенит</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Фото */}
+            <div className={classes.centerBlock}>
+                <div className={classes.videos}>
+                    <div className={classes.statistic_title}>
+                        <div className={classes.title_block}>Фото</div>
+                        <div className={classes.statistic_title_moreLink}>Все фото <img src="/arrowRight.png" alt="" /></div>
+                    </div>
+
+                    <div className={classes.videos_items}>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="photoPreview.png" alt="" />
+                                <div className={classes.news_items_elem_newInfo}>
+                                    <img src="photos.png" alt="" />
+                                    38 фото
+                                </div>
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Зенит 2-0 Локомотив</div>
+                        </div>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="photoPreview.png" alt="" />
+                                <div className={classes.news_items_elem_newInfo}>
+                                    <img src="photos.png" alt="" />
+                                    38 фото
+                                </div>
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Зенит 2-0 Локомотив</div>
+                        </div>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="photoPreview.png" alt="" />
+                                <div className={classes.news_items_elem_newInfo}>
+                                    <img src="photos.png" alt="" />
+                                    38 фото
+                                </div>
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Зенит 2-0 Локомотив</div>
+                        </div>
+                        <div className={classes.videos_items_item}>
+                            <div className={classes.videos_items_item_img}>
+                                <img src="photoPreview.png" alt="" />
+                                <div className={classes.news_items_elem_newInfo}>
+                                    <img src="photos.png" alt="" />
+                                    38 фото
+                                </div>
+                            </div>
+                            <div className={classes.videos_items_item_info}>
+                                <div className={classes.videos_items_item_info_date}>Пн, 18.08.2025</div>
+                            </div>
+
+                            <div className={classes.videos_items_item_info_name}>Зенит 2-0 Локомотив</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Клубы МЛФ Лиги */}
+            <div className={classes.centerBlock}>
+                <div className={classes.clubs}>
+                    <div className={classes.title_block}>Клубы МЛФ Лиги</div>
+
+                    <div className={classes.clubs_items}>
+                        <div className={classes.clubs_items_item}><img src="club1.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club2.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club3.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club4.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club1.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club2.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club3.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club4.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club1.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club2.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club3.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club4.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club1.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club2.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club3.png" alt="" /></div>
+                        <div className={classes.clubs_items_item}><img src="club4.png" alt="" /></div>
+                    </div>
                 </div>
             </div>
         </>
